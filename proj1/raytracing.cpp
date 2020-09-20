85d228896ed5e3e4dfe81f15792ae6246d00e1e9
Jason Carey <jcarey3@umbc.edu>
2020-09-19 20:57:01 -0400

    Fixed the issues with tracing the ray. The problem was in the intersect function of the Triangle class it turns out r.dir should not have been negated and when checking if the ray was inside of the triangle beta must also be less than 1 and gamma must be greater than 0. Fixed the pixelWidth so the produced image is no longer tiny, fixing this flipped the resulting image. Fixed the flipping issue by flipping it back before outputting the image. Added comments

--- proj1/raytracing.cpp
+++ proj1/raytracing.cpp
@@ -5,7 +5,7 @@
 
 struct hitRecord{
     Eigen::Vector3d color;
-    int t;
+    double t;
     bool inside;
 };
 struct ray{
@@ -14,29 +14,37 @@ struct ray{
 };
 class Geo{
    public:
+        //Should check if the ray r intersects the geo
         virtual hitRecord intersect(ray r)=0;
 };
+//A triangle class based off Geo that implements an intersect function
 class Triangle: public Geo{
     public:
+        /**
+         *Creates a triangle with the vertices vert and the color color 
+         *@param vert a size 3 array of Eigen::Vector3ds each specifying a vertex
+         *@param color an Eigen::Vector3d specifying the color of the Triangle
+         */
         Triangle(Eigen::Vector3d vert[3],Eigen::Vector3d color){
             this->vert[0]=vert[0];
             this->vert[1]=vert[1];
             this->vert[2]=vert[2];
             this->color=color;
         }
+        /**
+         * Checks to see if the ray r intersects with the triangle
+         * @param r the ray that is being tested
+         * @return hitRecord the hitRecord of r hitting the triangle
+         */
         hitRecord intersect(ray r){
             hitRecord hr;
-            Eigen::Matrix3d m;
-            // m<<(vert[1]-vert[0]),(vert[2]-vert[0]),(-r.dir);
-            m.col(0)=vert[1]-vert[0];
-            m.col(1)=vert[2]-vert[0];
-            m.col(2)=-r.dir;
-            Eigen::Vector3d sol=m.colPivHouseholderQr().solve(r.from-vert[0]);
-            //std::cout<<m<<std::endl;
+            Eigen::Matrix3d m;//A matrix that can be solved to find beta gamma and t
+            m<<(vert[1]-vert[0]),(vert[2]-vert[0]),((r.dir));//the columns of the matrix from the equation from-v0=beta(v1-v0)+gamma(v2-v0)+td
+            Eigen::Vector3d sol=m.colPivHouseholderQr().solve((r.from-vert[0]));//solves the matrix and stores beta,gamma,and t into sol
             hr.t=sol[2];
-            if(sol[0]>0&&sol[1]<1&&(sol[0]+sol[1]<1)){
-                hr.color=color;
-                hr.inside=true;
+            if(sol[0]>0&&sol[0]<1&&sol[1]>0&&sol[1]<1&&(sol[0]+sol[1]<1)){//if beta>0 &&beta<1 && gamma>0 &&beta<1 &&beta+gamma<1
+                hr.color=color;//sets the color of the hitrecord to the color of the triangle
+                hr.inside=true;//the ray ended inside the triangle
                 // std::cout<<"Hit!"<<hr.t<<std::endl;
             }
             else{
@@ -49,17 +57,20 @@ class Triangle: public Geo{
         Eigen::Vector3d color;
 };
 
-std::vector<Geo*> polygons;
-Eigen::Vector3d background;
-Eigen::Vector3d fill;
+std::vector<Geo*> polygons;//vector of polygons in the image
+Eigen::Vector3d background;//background color
+Eigen::Vector3d fill;//fill color
 Eigen::Vector3d from;
 Eigen::Vector3d at;
 Eigen::Vector3d up;
 Eigen::Vector3d w,u,v;
 double imgHalfWidth,pixelWidth,aspect,angle,hither;
 int resx,resy;
-bool nextTri,nextSphere,nextPoly,nextView;
-
+bool nextTri,nextSphere,nextPoly,nextView;//signals readInput that a multi line section is being read in
+/**
+ * Reads in the input from the specified .nff file
+ * @param in the .nff file to be read
+ */
 void readInput(std::string in){
     int numVert;
     std::vector<Eigen::Vector3d> vertexs;
@@ -131,6 +142,12 @@ void readInput(std::string in){
     }
     fil.close();
 }
+/**
+ * Calculates a ray for a pixel(j,i)
+ * @param i the y value of the pixel
+ * @param j the x value of the pixel
+ * @return r the ray that was calculated
+ */
 ray calcRay(int i,int j){
     double m=(-imgHalfWidth+pixelWidth/2.0)+j*pixelWidth;
     double n=-(imgHalfWidth+pixelWidth/2.0)/aspect+i*pixelWidth;
@@ -140,15 +157,20 @@ ray calcRay(int i,int j){
     r.from=from;
     return r;
 }
+/**
+ * Traces a ray and returns the appropriate color
+ * @param r the ray to be traced
+ * @return color an Eigen::Vector3d that corresponds to the color of the pixel
+ */
 Eigen::Vector3d trace(ray r){
-    int min_t=INT32_MAX;
+    double min_t=INFINITY;
     Eigen::Vector3d color=background;
     for(int i=0;i<polygons.size();i++){
         hitRecord hr=polygons.at(i)->intersect(r);
         if(hr.t<min_t&&hr.t>0&&hr.inside){
             min_t=hr.t;
             color=hr.color;
-            // std::cout<<polygons.size()<<std::endl;
+            // std::cout<<"INSIDE!"<<min_t<<std::endl;
         }
     }
     return color;
@@ -164,6 +186,12 @@ int main(int argc, char* argv[]){
     {
         std::cout<<"No output file detected, using default output file: output.ppm"<<std::endl;
     }
+    std::string outfile="output.ppm";//default output file
+    if(argc==3){
+        outfile=argv[2];//changes output file if one is specifide
+    }
+    char out[outfile.size()+1];
+    std::strcpy(out,outfile.c_str());
     readInput(argv[1]);
     unsigned char pixels[resy][resx][3];
     Eigen::Vector3d color;
@@ -171,8 +199,9 @@ int main(int argc, char* argv[]){
     u=up.cross(w).normalized();
     v=w.cross(u);
     imgHalfWidth=tan(M_PI*angle/180.0/2.0)*hither;
-    pixelWidth=(imgHalfWidth*2)/resx*2.0;
-    aspect=resx/resy;
+    pixelWidth=(imgHalfWidth*2)/double(resx);
+    aspect=double(resx)/double(resy);
+    //for every pixel it calculates a ray and then traces it assigning the color to the appropriate pixels rgb channels
     for(int i=0;i<resy;i++){
         for(int j=0;j<resx;j++){
             ray r=calcRay(i,j);
@@ -182,10 +211,21 @@ int main(int argc, char* argv[]){
             pixels[i][j][2]=color[2]*255;
         }
     }
-    FILE *f = fopen("output.ppm","wb");
+    //Not quite sure why but tetra-3 was comming out flipped so this flips it back
+    unsigned char pixelsFlipped[resy][resx][3];
+     for(int i=0;i<resy;i++){
+        for(int j=resx-1;j>=0;j--){
+            pixelsFlipped[i][j][0]=pixels[i][resx-(j+1)][0];
+            pixelsFlipped[i][j][1]=pixels[i][resx-(j+1)][1];
+            pixelsFlipped[i][j][2]=pixels[i][resx-(j+1)][2];
+        }
+    }
+    //Writes the pixels to the specified ppm file
+    FILE *f = fopen(out,"wb");
     fprintf(f, "P6\n%d %d\n%d\n", resx, resy, 255);
-    fwrite(pixels, 1, resx*resy*3, f);
+    fwrite(pixelsFlipped, 1, resx*resy*3, f);
     fclose(f);
+    //goes through and deallocates memory for the polygons
     while(!polygons.empty()){
         Geo* temp;
         temp=polygons.back();
