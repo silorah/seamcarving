b037f34c2f5886619eb5baf3f0ff101a0f95dc6e
Jason Carey <jcarey3@umbc.edu>
2020-12-05 18:12:18 -0500

    Implemented blending Added transform to vertex processing Added limit in rasterization loop minx/y to maxX/y or resx/resy

--- proj3/rasterizer.cpp
+++ proj3/rasterizer.cpp
@@ -23,43 +23,50 @@ double det(const Eigen::Vector3d &a, const Eigen::Vector3d &b, const Eigen::Vect
 }
 
 inline double sqr(double x) {return x*x;}
-inline Eigen::Vector3d cross(const Eigen::Vector3d &x, const Eigen::Vector3d &y) {return x.cross(y);}
+//inline Eigen::Vector4d cross(const Eigen::Vector4d &x, const Eigen::Vector4d &y) {return x.cross(y);}
 inline double dot(const Eigen::Vector3d &x, const Eigen::Vector3d &y) {return x.dot(y);}
 inline double cross2d(const Eigen::Vector2d &x, const Eigen::Vector2d &y) {return x[0]*y[1]-x[1]*y[0];}
-
-Eigen::Vector4d Triangle::normal(const Eigen::Vector4d &x) const {
-	Eigen::Vector4d n = cross(b-a, c-a);
-	n.normalize();
-	return n;
-}
-void Triangle::transform(Eigen::Matrix4d m){
+// Eigen::Vector4d cross4d(Eigen::Vector4d &x,Eigen::Vector4d &y){
+// 	Eigen::Vector4d res;
+// 	res[0]=
+// 	res[1]=
+// 	res[2]=
+// 	res[3]=
+// }
+// Eigen::Vector4d Triangle::normal(const Eigen::Vector4d &x) const {
+// 	Eigen::Vector4d n = cross(b-a, c-a);
+// 	n.normalize();
+// 	return n;
+// }
+void Triangle::transform(const Eigen::Matrix4d &m){
 	ap=m*a;
 	bp=m*b;
 	cp=m*c;
+	// std::cout<<"\na"<<a<<"\naP"<<ap<<"\nb"<<b<<"\nbp"<<bp<<"\nc"<<c<<"\ncP"<<cp<<std::endl;
 }
-Eigen::Vector4d Triangle/*Patch*/::normal(const Eigen::Vector4d &x) const {
-	Eigen::Vector4d n = cross(b-a, c-a);
-	double area = n.norm();
-	n = cross(x-a, c-a);
-	double alpha = n.norm()/area;
-	n = cross(x-a, b-a);
-	double beta = n.norm()/area;
-	n = (1-alpha-beta)*n1 + alpha*n2 + beta*n3;
-	n.normalize();
-	return n;
-}
+// Eigen::Vector4d Triangle/*Patch*/::normal(const Eigen::Vector4d &x) const {
+// 	Eigen::Vector4d n = cross(b-a, c-a);
+// 	double area = n.norm();
+// 	n = cross(x-a, c-a);
+// 	double alpha = n.norm()/area;
+// 	n = cross(x-a, b-a);
+// 	double beta = n.norm()/area;
+// 	n = (1-alpha-beta)*n1 + alpha*n2 + beta*n3;
+// 	n.normalize();
+// 	return n;
+// }
 
-Eigen::Vector4d Poly::normal(const Eigen::Vector4d &x) const {
-	Eigen::Vector4d n = cross(verts[1]-verts[0],verts[2]-verts[0]);
-	n.normalize();
-	return n;
-}
-void Poly::transform(Eigen::Matrix4d m){
+// Eigen::Vector4d Poly::normal(const Eigen::Vector4d &x) const {
+// 	Eigen::Vector4d n = cross(verts[1]-verts[0],verts[2]-verts[0]);
+// 	n.normalize();
+// 	return n;
+// }
+void Poly::transform(Eigen::Matrix4d &m){
 	for(int i=0;i<verts.size();i++){
 		vertsP.push_back(m*verts[i]);
 	}
 }
-Camera createCamera(Eigen::Vector3d eye, Eigen::Vector3d at,Eigen::Vector3d up){
+Camera createCamera(Eigen::Vector3d &eye, Eigen::Vector3d &at,Eigen::Vector3d &up){
 	Camera cam;
 	//cam=new Camera;
 	cam.eye=eye;
@@ -152,18 +159,18 @@ Renderer::Renderer(const std::string &fname) {
 			triangles.push_back(std::pair<Triangle *, Fill>(new Triangle(vertices[0], vertices[1], vertices[2], 
 						normals[0], normals[1], normals[2]), fill));
 		} else {
-			Eigen::Vector4d n0 = cross(vertices[1] - vertices[0], vertices[2] - vertices[0]);
-			Eigen::Vector4d n1 = cross(vertices[2] - vertices[1], vertices[0] - vertices[1]);
-			Eigen::Vector4d n2 = cross(vertices[0] - vertices[2], vertices[1] - vertices[2]);
+			Eigen::Vector4d n0 = (vertices[1] - vertices[0]).cross3(vertices[2] - vertices[0]);
+			Eigen::Vector4d n1 = (vertices[2] - vertices[1]).cross3(vertices[0] - vertices[1]);
+			Eigen::Vector4d n2 = (vertices[0] - vertices[2]).cross3(vertices[1] - vertices[2]);
 			//surfaces.push_back(std::pair<Surface *, Fill>(new Triangle(vertices[0], vertices[1], vertices[2],n0,n1,n2), fill));
 			triangles.push_back(std::pair<Triangle *, Fill>(new Triangle(vertices[0], vertices[1], vertices[2],n0,n1,n2), fill));
 		}
 		} else if (vertices.size() == 4) {
-		Eigen::Vector3d n0 = cross(vertices[1] - vertices[0], vertices[2] - vertices[0]);
-		Eigen::Vector3d n1 = cross(vertices[2] - vertices[1], vertices[3] - vertices[1]);
-		Eigen::Vector3d n2 = cross(vertices[3] - vertices[2], vertices[0] - vertices[2]);
-		Eigen::Vector3d n3 = cross(vertices[0] - vertices[3], vertices[1] - vertices[3]);
-		if (dot(n0,n1) > 0 && dot(n0,n2) > 0 && dot(n0,n3) > 0) {
+		Eigen::Vector4d n0 = (vertices[1] - vertices[0]).cross3(vertices[2] - vertices[0]);
+		Eigen::Vector4d n1 = (vertices[2] - vertices[1]).cross3(vertices[3] - vertices[1]);
+		Eigen::Vector4d n2 = (vertices[3] - vertices[2]).cross3(vertices[0] - vertices[2]);
+		Eigen::Vector4d n3 = (vertices[0] - vertices[3]).cross3(vertices[1] - vertices[3]);
+		if (n0.dot(n1) > 0 && n0.dot(n2) > 0 && n0.dot(n3) > 0) {
 			makeTriangles = true;
 			if (patch) {
 			// surfaces.push_back(std::pair<Surface *, Fill>(new Triangle(vertices[0], vertices[1], vertices[2], 
@@ -234,6 +241,7 @@ Renderer::Renderer(const std::string &fname) {
 Renderer::~Renderer() {
 	if (im) delete [] im;
 	for (unsigned int i=0; i<surfaces.size(); i++) delete surfaces[i].first;
+	for (unsigned int i=0;i<triangles.size();i++) delete triangles[i].first;
 }
 void Renderer::vertexProcessing(std::pair<Triangle*,Fill> s,Camera cam){
 	Triangle *tri=s.first;
@@ -241,6 +249,7 @@ void Renderer::vertexProcessing(std::pair<Triangle*,Fill> s,Camera cam){
 	Eigen::Vector4d v[3];
 	Eigen::Vector4d eye;
 	Eigen::Vector3d color[3];
+	tri->transform(M);
 	color[0]<<0.0,0.0,0.0;
 	color[1]<<0.0,0.0,0.0;
 	color[2]<<0.0,0.0,0.0;
@@ -268,13 +277,13 @@ void Renderer::vertexProcessing(std::pair<Triangle*,Fill> s,Camera cam){
 		h[1].normalize();
 		h[2].normalize();
 		double diffuse[3];
-		diffuse[0]= std::max(0.0, dot(tri->n1, l[0]));
-		diffuse[1]= std::max(0.0, dot(tri->n2, l[1]));
-		diffuse[2]= std::max(0.0, dot(tri->n3, l[2]));
+		diffuse[0]= std::max(0.0, (tri->n1).dot(l[0]));
+		diffuse[1]= std::max(0.0, (tri->n2).dot(l[1]));
+		diffuse[2]= std::max(0.0, (tri->n3).dot(l[2]));
 		double specular[3];
-		specular[0]=pow(std::max(0.0, dot(tri->n1, h[0])), f.shine);
-		specular[1]=pow(std::max(0.0, dot(tri->n2, h[1])), f.shine);
-		specular[2]=pow(std::max(0.0, dot(tri->n3, h[2])), f.shine);
+		specular[0]=pow(std::max(0.0, (tri->n1).dot(h[0])), f.shine);
+		specular[1]=pow(std::max(0.0, (tri->n2).dot(h[1])), f.shine);
+		specular[2]=pow(std::max(0.0, (tri->n3).dot(h[2])), f.shine);
 		Eigen::Vector3d c[3];
 		c[0]=(f.kd * diffuse[0] * f.color + f.ks * specular[0] * Eigen::Vector3d(1.0,1.0,1.0));
 		c[1]=(f.kd * diffuse[1] * f.color + f.ks * specular[1] * Eigen::Vector3d(1.0,1.0,1.0));
@@ -290,9 +299,10 @@ void Renderer::vertexProcessing(std::pair<Triangle*,Fill> s,Camera cam){
 	tri->c1=color[1];
 	tri->c2=color[2];
 }
-void Renderer::rasterization(std::pair<Triangle*,Fill> t, int resy){
+void Renderer::rasterization(std::pair<Triangle*,Fill> t,int resx, int resy){
 	int minX,minY,maxX,maxY;
 	Triangle *tri=t.first;
+	// std::cout<<"a"<<tri->ap<<"b"<<tri->bp<<"c"<<tri->cp<<std::endl;
 	//Find min and max xvals for bounding box
 	tri->ap[0]/=tri->ap[3];
 	tri->ap[1]/=tri->ap[3];
@@ -365,49 +375,95 @@ void Renderer::rasterization(std::pair<Triangle*,Fill> t, int resy){
 	v0<<tri->ap[0],tri->ap[1];
 	v1<<tri->bp[0],tri->bp[1];
 	v2<<tri->cp[0],tri->cp[1];
-	Eigen::Vector3d n=cross(v1-v0,v2-v0);
-	double area=n.norm();
+	// 2dcross(v1-v0,v2-v0);
 	double alpha,beta,gamma;
-	for(int i=minX;i<maxX;i++){
-		for(int j=minY;j<maxY;j++){
+	Eigen::Vector3d color;
+	// std::cout<<"minx "<<minX<<" maxx "<<maxX<<" miny "<<minY<<" maxy "<<maxY<<std::endl;
+	for(unsigned int i=minX;i<maxX&&i<resx;i++){
+		for(unsigned int j=minY;j<maxY&&j<resy;j++){
 			p<<i,j;
-			n=cross(v1-p,v2-p);
-			alpha=n.norm()/area;
-			n=cross(v0-p,v2-p);
-			beta=n.norm()/area;
-			n=cross(v1-p,v0-p);
-			gamma=n.norm()/area;
+			alpha=((v1[1]-v2[1])*p[0] + (v2[0]-v1[0])*p[1] +(v1[0]*v2[1])-(v2[0]*v1[1]))/((v1[1]-v2[1])*v0[0] + (v2[0]-v1[0])*v0[1] +(v1[0]*v2[1])-(v2[0]*v1[1]));
+			beta=((v2[1]-v0[1])*p[0] + (v0[0]-v2[0])*p[1] +(v2[0]*v0[1])-(v0[0]*v2[1]))/((v2[1]-v0[1])*v1[0] + (v0[0]-v2[0])*v1[1] +(v2[0]*v0[1])-(v0[0]*v2[1]));;
+			gamma=((v0[1]-v1[1])*p[0] + (v1[0]-v0[0])*p[1] +(v0[0]*v1[1])-(v1[0]*v0[1]))/((v0[1]-v1[1])*v2[0] + (v1[0]-v0[0])*v2[1] +(v0[0]*v1[1])-(v1[0]*v0[1]));;
 			if(alpha>0&&beta>0&&gamma>0){
-				fragments[i*resy+j].push_back(Fragment(alpha*tri->ap[2]+beta*tri->bp[2]+gamma*tri->cp[2],alpha*tri->c0+beta*tri->c1+gamma*tri->c2));
+				color=alpha*tri->c0+beta*tri->c1+gamma*tri->c2;
+				fragments[i*resy+j].push_back(Fragment((alpha*tri->ap[3])+(beta*tri->bp[3])+(gamma*tri->cp[3]),color));
 			}
+		}
+	}
+	// std::cout<<"EoR"<<std::endl;
+}
+void Renderer::blending(int resx,int resy){
+	double minZ;
+	Eigen::Vector3d color;
+	for(unsigned int i=0;i<resx;i++){
+		for(unsigned int j=0;j<resy;j++){
+			minZ=DBL_MAX;
+			color=bcolor;
+			// std::cout<<fragments[i*resy+j].size()<<std::endl;
+			for(unsigned int f=0;f<fragments[i*resy+j].size();f++){
+				if(fragments[i*resy+j][f].z<minZ){
+					// std::cout<<"What the Fuck!"<<std::endl;
+					minZ=fragments[i*resy+j][f].z;
+					color=fragments[i*resy+j][f].color;
+				}
+			}
+			im[i*resy+j]=color;
 		}
 	}
 }
 void Renderer::render(Camera cam,int resx,int resy){
-	for(int i=0;i<triangles.size();i++){
+	std::cout<<"VP"<<std::endl;
+	for(unsigned int i=0;i<triangles.size();i++){
 		vertexProcessing(triangles[i],cam);
-		rasterization(triangles[i],resy);
+		std::cout<<"Raster "<<i<<std::endl;
+		rasterization(triangles[i],resx,resy);
 	}
+	std::cout<<"EoR"<<std::endl;
+	std::cout<<"blending"<<std::endl;
+	blending(resx,resy);
 }
-void Renderer::setUpM(Camera cam, double l, double r, double t, double b, double n, double resx,double resy,Eigen::Vector3d u, Eigen::Vector3d v, Eigen::Vector3d w){
+void Renderer::setUpM(Camera cam, double l, double r, double t, double b, double n, double resx,double resy,const Eigen::Vector3d &u, const Eigen::Vector3d &v, const Eigen::Vector3d &w){
 	Eigen::Matrix4d Mcam,Mvp,Mper,Morth,eyeinv;
+	std::cout<<cam.eye<<std::endl;
 	double f=1000*n;
-	Mcam<<u[0],u[1],u[2],0.0,v[0],v[1],v[2],0.0,w[0],w[1],w[2],0.0,0.0,0.0,0.0,1.0;
-	eyeinv<<1.0,0.0,0.0,-cam.eye[0],0.0,1.0,0.0,-cam.eye[1],0.0,0.0,1.0,-cam.eye[2],0.0,0.0,0.0,1.0;
+	Mcam<<u[0],u[1],u[2],0.0,
+		v[0],v[1],v[2],0.0,
+		w[0],w[1],w[2],0.0,
+		0.0,0.0,0.0,1.0;
+	eyeinv<<1.0,0.0,0.0,-cam.eye[0],
+		0.0,1.0,0.0,-cam.eye[1],
+		0.0,0.0,1.0,-cam.eye[2],
+		0.0,0.0,0.0,1.0;
 	Mcam=Mcam*eyeinv;
-	Morth<<2/(r-l),0.0,0.0,-((r+l)/(r-l)),0.0,2/(t-b),0.0,-((t+b)/(t-b)),0.0,0.0,2/(n-f),-((n+f)/(n-f)),0.0,0.0,0.0,1.0;
-	Mper<<n,0.0,0.0,0.0,0.0,n,0.0,0.0,0.0,0.0,n+f,-(f*n),0.0,0.0,1.0,0.0;
+	Morth<<(2/(r-l)), 0.0, 0.0, (-(r+l)/(r-l)),
+		0.0, (2/(t-b)), 0.0, (-(t+b)/(t-b)),
+		0.0, 0.0, (2/(n-f)), (-(n+f)/(n-f)),
+		0.0, 0.0, 0.0, 1.0;
+	Mper<<n,0.0,0.0,0.0,
+		0.0,n,0.0,0.0,
+		0.0,0.0,n+f,f*n,
+		0.0,0.0,1.0,0.0;
 	Mper=Morth*Mper;
-	Mvp<<resx/2.0,0.0,0.0,(resx-1)/2.0,0.0,resy/2.0,0.0,(resy-1)/2.0,0.0,0.0,1.0,0.0,0.0,0.0,0.0,1.0;
+	Mper<<-2.41421,0,0,0,0,-2.41421,0,0,0,0,-1.002,-2.002,0,0,1,0;
+	Mvp<<resx/2.0,0.0,0.0,(resx-1)/2.0,
+		0.0,resy/2.0,0.0,(resy-1)/2.0,
+		0.0,0.0,1.0,0.0,
+		0.0,0.0,0.0,1.0;
 	M=Mvp*Mper*Mcam;
+	std::cout<<"Mvp=\n"<<Mvp<<std::endl;
+	std::cout<<"Mper=\n"<<Mper<<std::endl;
+	std::cout<<"Mcam=\n"<<Mcam<<std::endl;
+	std::cout<<"M=\n"<<M<<std::endl;
+	
 }
 void Renderer::createImage() {
 	// set up coordinate system
 	Eigen::Vector3d w = eye - at;
 	w /= w.norm();
-	Eigen::Vector3d u = cross(up,w);
+	Eigen::Vector3d u = up.cross(w);
 	u.normalize();
-	Eigen::Vector3d v = cross(w,u);
+	Eigen::Vector3d v = w.cross(u);
 
 	//std::cout<<u<<" "<<v<<" "<<w<<std::endl;
 
@@ -428,9 +484,15 @@ void Renderer::createImage() {
 	}
 	Camera cam;
 	fragments=new std::vector<Fragment>[res[0]*res[1]];
+	std::cout<<"Camera"<<std::endl;
 	cam= createCamera(eye,at,up);
+	std::cout<<"setUpM"<<std::endl;
+	std::cout<<"l:"<<l<<std::endl;
+	std::cout<<"t:"<<t<<std::endl;
 	Renderer::setUpM(cam,l,-l,t,-t,hither,res[0],res[1],u,v,w);
+	std::cout<<"Render"<<std::endl;
 	Renderer::render(cam,res[0],res[1]);
+	std::cout<<"ended render"<<std::endl;
 
 }
 
@@ -542,6 +604,7 @@ int main(int argc, char *argv[]) {
 	renderer.shadows = shadows;
 	renderer.maxraydepth = maxraydepth;
 	renderer.verbose = verbose;
+	std::cout<<"creating"<<std::endl;
 	renderer.createImage();
 	std::cout<<"writing "<<argv[optind]<<std::endl;
 	renderer.writeImage(argv[optind++]);
--- proj3/rasterizer.h
+++ proj3/rasterizer.h
@@ -8,12 +8,14 @@
 
 class Fill {
 public: 
+	EIGEN_MAKE_ALIGNED_OPERATOR_NEW
 	Eigen::Vector3d color;
 	double kd, ks, shine, transmittance, ior;
 };
 
 class HitRecord {
 public:
+	EIGEN_MAKE_ALIGNED_OPERATOR_NEW
 	double t, alpha, beta, gamma;
 	Eigen::Vector3d p, n, v;
 	Fill f;
@@ -28,8 +30,8 @@ public:
 
 class Surface {
 public:
-	virtual Eigen::Vector4d normal(const Eigen::Vector4d &x) const = 0;
-	virtual void transform(Eigen::Matrix4d m);
+	//virtual Eigen::Vector4d normal(const Eigen::Vector4d &x) const = 0;
+	//virtual void transform(Eigen::Matrix4d m);
 	virtual ~Surface() {};
 };
 
@@ -43,36 +45,41 @@ public:
 
 class Triangle : public Surface {	
 public:
+	EIGEN_MAKE_ALIGNED_OPERATOR_NEW
 	Eigen::Vector4d n1, n2, n3,a,b,c,ap,bp,cp;
 	Eigen::Vector3d c0,c1,c2;
 	Triangle(const Eigen::Vector4d &_a, const Eigen::Vector4d &_b, const Eigen::Vector4d &_c,
 		const Eigen::Vector4d &_n1, const Eigen::Vector4d &_n2, const Eigen::Vector4d &_n3) 
 	: /*Triangle(_a,_b,_c)*/a(_a), b(_b), c(_c), n1(_n1), n2(_n2), n3(_n3) {};
-	virtual Eigen::Vector4d normal(const Eigen::Vector4d &x) const;
-	virtual void transform(Eigen::Matrix4d m);
+	//virtual Eigen::Vector4d normal(const Eigen::Vector4d &x) const;
+	virtual void transform(const Eigen::Matrix4d &m);
 };
 
 class Poly : public Surface {
 public:
+	EIGEN_MAKE_ALIGNED_OPERATOR_NEW
 	std::vector<Eigen::Vector4d> verts;
 	std::vector<Eigen::Vector4d> vertsP;
 	Poly(const std::vector<Eigen::Vector4d> &_verts) : verts(_verts) {};
-	virtual void transform(Eigen::Matrix4d m); 
-	virtual Eigen::Vector4d normal(const Eigen::Vector4d &x) const;
+	virtual void transform(Eigen::Matrix4d &m); 
+	//virtual Eigen::Vector4d normal(const Eigen::Vector4d &x) const;
 };
 
 class PolyPatch : public Poly {
 	std::vector<Eigen::Vector4d> normals;
 public:
+	EIGEN_MAKE_ALIGNED_OPERATOR_NEW
 	PolyPatch(const std::vector<Eigen::Vector4d> &_verts, const std::vector<Eigen::Vector4d> &_normals) : Poly(_verts), normals(_normals) {}; 
 };
 class Camera{
 	public:
+		EIGEN_MAKE_ALIGNED_OPERATOR_NEW
 		Eigen::Vector3d eye,at,up;
 };
 class Fragment{
 	public:
-	Fragment(double _z,Eigen::Vector3d c):z(_z),color(c){};
+	EIGEN_MAKE_ALIGNED_OPERATOR_NEW
+	Fragment(double _z,const Eigen::Vector3d &c):z(_z),color(c){};
 		double z;
 		Eigen::Vector3d color;
 };
@@ -87,12 +94,13 @@ class Renderer {
 
 	Eigen::Vector3d *im;
 public:
+	EIGEN_MAKE_ALIGNED_OPERATOR_NEW
 	Renderer(const std::string &fname);
 	~Renderer();
 	void createImage();
-	void setUpM(Camera cam, double l, double r, double t, double b, double n,double resx,double resy, Eigen::Vector3d u, Eigen::Vector3d v, Eigen::Vector3d w);  
+	void setUpM(Camera cam, double l, double r, double t, double b, double n,double resx,double resy, const Eigen::Vector3d &u, const Eigen::Vector3d &v, const Eigen::Vector3d &w);  
 	void vertexProcessing(std::pair<Triangle*,Fill> s,Camera cam);
-	void rasterization(std::pair<Triangle*,Fill> t,int resy);
+	void rasterization(std::pair<Triangle*,Fill> t,int resx,int resy);
 	void fragmentProcessing();
 	void blending(int resx,int resy);
 	void clipping();
