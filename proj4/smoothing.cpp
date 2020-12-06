7ab9f230aff260c309da2ca8667418707b0f5db4
Jason Carey <jcarey3@umbc.edu>
2020-12-06 00:48:42 -0500

    created a laplacian operator, main loop now calls that instead of doing it in place

--- proj4/smoothing.cpp
+++ proj4/smoothing.cpp
@@ -1,10 +1,39 @@
 #include "io.cpp"
+void laplacian(const std::vector<Tri> &triangles, const std::vector<Eigen::Vector3d> &x, std::vector<Eigen::Vector3d> &y){
+	std::vector<double> m;
+    std::vector<Eigen::Vector3d> lap;
+	for(int j=0;j<x.size();j++){
+        m.push_back(0);
+        lap.push_back(Eigen::Vector3d(0,0,0));
+    }
+	for(int j=0;j<x.size();j++){
+        	m[j]=0;
+            lap[j]=Eigen::Vector3d(0,0,0);
+        }
+
+        for(int t=0;t<triangles.size();t++){
+            lap[triangles[t][0]]+=x[triangles[t][1]]-x[triangles[t][0]];
+            lap[triangles[t][0]]+=x[triangles[t][2]]-x[triangles[t][0]];
+
+            lap[triangles[t][1]]+=x[triangles[t][0]]-x[triangles[t][1]];
+            lap[triangles[t][1]]+=x[triangles[t][2]]-x[triangles[t][1]];
+
+            lap[triangles[t][2]]+=x[triangles[t][0]]-x[triangles[t][2]];
+            lap[triangles[t][2]]+=x[triangles[t][1]]-x[triangles[t][2]];
+
+            m[triangles[t][0]]+=2;
+            m[triangles[t][1]]+=2;
+            m[triangles[t][2]]+=2;
+        }
+        for(int j=0;j<x.size();j++){
+			y[j]=lap[j]/m[j];
+		}
+}
 int main(int argc, char* argv[]){
-    std::vector<Eigen::Vector3d> pts;
+    std::vector<Eigen::Vector3d> pts,y;
     std::vector<Tri> triangles;
     int n;
-    std::vector<double> m;//mass
-    std::vector<Eigen::Vector3d> lap;//laplacian op
+
     readObjFile(argv[1],pts,triangles);
     std::cout<<"in"<<argv[1]<<std::endl;
     std::cout<<"out"<<argv[2]<<std::endl;
@@ -13,31 +42,12 @@ int main(int argc, char* argv[]){
     std::cout<<n<<std::endl;
     
     for(int j=0;j<pts.size();j++){
-        m.push_back(0);
-        lap.push_back(Eigen::Vector3d(0,0,0));
+        y.push_back(Eigen::Vector3d(0,0,0));
     }
     for(int i =0;i<n;i++){
+       	laplacian(triangles,pts,y);
         for(int j=0;j<pts.size();j++){
-            m[j]=0;
-            lap[j]=Eigen::Vector3d(0,0,0);
-        }
-
-        for(int t=0;t<triangles.size();t++){
-            lap[triangles[t][0]]+=pts[triangles[t][1]]-pts[triangles[t][0]];
-            lap[triangles[t][0]]+=pts[triangles[t][2]]-pts[triangles[t][0]];
-
-            lap[triangles[t][1]]+=pts[triangles[t][0]]-pts[triangles[t][1]];
-            lap[triangles[t][1]]+=pts[triangles[t][2]]-pts[triangles[t][1]];
-
-            lap[triangles[t][2]]+=pts[triangles[t][0]]-pts[triangles[t][2]];
-            lap[triangles[t][2]]+=pts[triangles[t][1]]-pts[triangles[t][2]];
-
-            m[triangles[t][0]]+=2;
-            m[triangles[t][1]]+=2;
-            m[triangles[t][2]]+=2;
-        }
-        for(int j=0;j<pts.size();j++){
-            pts[j]+=atof(argv[3])*lap[j]/m[j];
+            pts[j]+=atof(argv[3])*y[j];
         }
     }
     std::cout<<"Writing"<<std::endl;
