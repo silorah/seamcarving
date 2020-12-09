ce2a607162e77c1499661477d08e7c2fbb3a4361
Jason Carey <jcarey3@umbc.edu>
2020-12-08 23:09:41 -0500

    removed code that was commented out

--- proj5/fishtank.cpp
+++ proj5/fishtank.cpp
@@ -11,7 +11,7 @@ class Boid{
     ~Boid(){};
     double mass;
     Eigen::Vector3d pos,vel;//position,velocity
-    Eigen::Vector3d netForce;//the net force acting on the boid calculated by mass*( collision*(colF/dt)+centering*(centF/dt)+velocity*(velF/dt)+vel/dt)
+    Eigen::Vector3d netForce;//the net force acting on the boid
 };
 class Animation{
     public:
@@ -28,13 +28,6 @@ void Animation::animate(){
     double time=0.0;
     double frameTime=-1.0;
     double distx,disty,distz,m;
-    // std::ofstream out;
-    // out.open("foo.text");
-    // for(int i=0;i<numBoids;i++){
-    //     out<<"["<<fish[i].pos[0]<<","<<fish[i].pos[1]<<","<<fish[i].pos[2]<<"] ";
-    //     out<<"["<<fish[i].vel[0]<<","<<fish[i].vel[1]<<","<<fish[i].vel[2]<<"]"<<std::endl;
-    // }
-    // out.close();
     Eigen::Vector3d avgPos=Eigen::Vector3d(0,0,0),avgVel=Eigen::Vector3d(0,0,0),colForce=Eigen::Vector3d(0,0,0);
     Boid curr,neigh;
     while(time<length){
@@ -51,29 +44,6 @@ void Animation::animate(){
                 disty=fabs(fish[j].pos[1]-fish[i].pos[1]);
                 distz=fabs(fish[j].pos[2]-fish[i].pos[2]);
                 if(j!=i&&n<numNeighbors&&(distx<=radius && disty<=radius && distz<=radius )){
-                    // if(distx>0&&disty>0&&distz>0){
-                    //     // centF[0]+=fish[j].pos[0]/(distx*distx);
-                    //     // centF[1]+=fish[j].pos[1]/(disty*disty);
-                    //     // centF[2]+=fish[j].pos[2]/(distz*distz);
-                    //     // colF[0]-=((fish[j].pos[0]-fish[i].pos[0])/(distx*distx));
-                    //     // colF[1]-=((fish[j].pos[1]-fish[i].pos[1])/(disty*disty));
-                    //     // colF[2]-=((fish[j].pos[2]-fish[i].pos[2])/(distz*distz));
-                    //     centF[0]+=(fish[j].pos[0]-fish[i].pos[0]);
-                    //     centF[1]+=(fish[j].pos[0]-fish[i].pos[1]);
-                    //     centF[2]+=(fish[j].pos[0]-fish[i].pos[2]);
-                    //     colF[0]-=(fish[j].pos[0]-fish[i].pos[0]);
-                    //     colF[1]-=(fish[j].pos[1]-fish[i].pos[1]);
-                    //     colF[2]-=(fish[j].pos[2]-fish[i].pos[2]);
-                    // }
-                    // else{                        
-                    //     centF[0]+=(fish[j].pos[0]-fish[i].pos[0]);
-                    //     centF[1]+=(fish[j].pos[0]-fish[i].pos[1]);
-                    //     centF[2]+=(fish[j].pos[0]-fish[i].pos[2]);
-                    //     colF[0]-=(fish[j].pos[0]-fish[i].pos[0]);
-                    //     colF[1]-=(fish[j].pos[1]-fish[i].pos[1]);
-                    //     colF[2]-=(fish[j].pos[2]-fish[i].pos[2]);
-                    
-                    // }
                     colForce=fish[i].pos-fish[j].pos;
                     m=colForce.norm();
                     if(m>.0001){
@@ -81,23 +51,10 @@ void Animation::animate(){
                     }
                     avgPos+=fish[j].pos;
                     avgVel+=fish[j].vel;
-                // fish[i].colF[0]*=sqrt(fabs(colF[0]-fish[i].pos[0]));
-                // fish[i].colF[1]*=sqrt(fabs(colF[1]-fish[i].pos[1]));
-                // fish[i].colF[2]*=sqrt(fabs(colF[2]-fish[i].pos[2]));
                     n++;
                 }
             }
             if(n>0){
-                // centF=(centF-fish[i].pos)/dt;
-                // velF=(velF-fish[i].vel);
-                // colF=(colF-fish[i].pos)/dt;
-                // centF=fish[i].mass*(centF/dt);
-                // colF=fish[i].mass*(colF/dt);
-                // velF=fish[i].mass*(velF/dt);
-                // centF/=n;
-                // colF/=n;
-                // velF/=n;
-                // centF/=centF.norm();
                 avgPos/=n;
                 avgVel/=n;
                 m=(avgPos-fish[i].pos).norm();
@@ -106,12 +63,6 @@ void Animation::animate(){
                 }
                 fish[i].netForce+=velocity*(avgVel-fish[i].vel);
             }
-            else{
-                // fish[i].colF=Eigen::Vector3d(0.0,0.0,0.0);
-                // fish[i].centF=Eigen::Vector3d(0.0,0.0,0.0);
-                // fish[i].velF=Eigen::Vector3d(0.0,0.0,0.0);
-            }
-            // fish[i].netForce=centering*avgPos+velocity*avgVel;
         }
         for(int i=0;i<numBoids;i++){
             fish[i].vel+=dt*(fish[i].netForce/fish[i].mass);
@@ -141,26 +92,6 @@ void Animation::animate(){
                 fish[i].pos[2]=zbound;
                 fish[i].vel[2]=-fabs(fish[i].vel[2]);
             }
-            // if(fabs(fish[i].pos[0])>=xbound){
-            //     fish[i].vel[0]*=-1;
-            // }
-            // if(fabs(fish[i].pos[1])>=ybound){
-            //     fish[i].vel[1]*=-1;
-            // }
-            // if(fabs(fish[i].pos[2])>=zbound){
-            //     fish[i].vel[2]*=-1;
-            // }
-            // if(fabs(fish[i].pos[0])>=xbound||fabs(fish[i].pos[1])>=ybound||fabs(fish[i].pos[2])>=zbound){
-            //     fish[i].vel*=-1;
-            // }
-            // fish[i].pos[0]=fish[i].pos[0]+fish[i].vel[0]*dt+(((fish[i].netForce[0]/mass)*(dt*dt))/2);
-            // fish[i].pos[0]=fish[i].pos[1]+fish[i].vel[1]*dt+(((fish[i].netForce[1]/mass)*(dt*dt))/2);
-            // fish[i].pos[0]=fish[i].pos[2]+fish[i].vel[2]*dt+(((fish[i].netForce[2]/mass)*(dt*dt))/2);
-            // // fish[i].vel[0]=(fish[i].vel[0])+(fish[i].velF[0]*velocity)+(fish[i].colF[0]*collision)+(fish[i].centF[0]*centering);
-            // // fish[i].vel[1]=(fish[i].vel[1])+(fish[i].velF[1]*velocity)+(fish[i].colF[1]*collision)+(fish[i].centF[1]*centering);
-            // // fish[i].vel[2]=(fish[i].vel[2])+(fish[i].velF[2]*velocity)+(fish[i].colF[2]*collision)+(fish[i].centF[2]*centering);
-            // fish[i].vel-=((fish[i].netForce/fish[i].mass)*dt);
-            // fish[i].vel*=damping;
         }
         time+=dt;
         frameTime-=dt;
@@ -174,7 +105,6 @@ bool Animation::read(const std::string &fname){
     in>>size>>radius>>numNeighbors>>mass>>collision>>centering>>velocity>>hunger>>damping>>dt>>length;
     std::cout<<size<<" "<<radius<<" "<<numNeighbors<<" "<<mass<<" "<<collision<<" "<<centering<<" "<<velocity<<" "<<hunger<<" "<<damping<<" "<<dt<<" "<<length<<std::endl;
     in>>numBoids;
-    // numBoids=0;
     int i=0;
     getline(in,line);
    while(in){
