7ae374ca5c0b837202bcd5f2d3a6cfcd5bd5a920
Jason Carey <jcarey3@umbc.edu>
2020-12-13 16:47:26 -0500

    Modified energyCalc so it also store energy in an array of doubles, added computeCost,not sure it works quite right began trying to output a seam to the image

--- proj6/stub.cpp
+++ proj6/stub.cpp
@@ -1,9 +1,74 @@
 #define cimg_display 0
 #include "CImg.h"
 #include <iostream>
+#include <vector>
 #include <Eigen/Dense>
 using namespace cimg_library;
-void energyCalc(Eigen::Vector3d *energy,Eigen::Vector3d *image,int width,int height){
+Eigen::Vector3d min(Eigen::Vector3d a,Eigen::Vector3d b,Eigen::Vector3d c){
+	Eigen::Vector3d min;
+	if (a[0]<b[0]&&a[0]<c[0]) min[0]=a[0];
+	if (a[1]<b[1]&&a[1]<c[1]) min[1]=a[1];
+	return a;
+}
+void computeCost(Eigen::Vector3d *energy,Eigen::Vector3d *cost,int width,int height){
+	int row;
+	for(unsigned int i=0;i<width;i++){
+		for(unsigned int j=0;j<height;j++){
+			if(i==0){
+				cost[i*height+j]=energy[i*height+j];
+			}
+			else{
+				row=(i-1)*height;
+				if(j!=0||j!=height-1){
+					cost[i*height+j]=energy[i*height+j];
+					if(cost[row+(j-1)].norm()<cost[row+j].norm()){
+						if(cost[row+(j-1)].norm()<cost[row+(j+1)].norm()){
+							cost[i*height+j]+=cost[row+(j-1)];
+						}
+						else{
+							cost[i*height+j]+=cost[row+(j+1)];
+						}
+					}
+					else if(cost[row+j].norm()<cost[row+(j+1)].norm()){
+						cost[i*height+j]+=cost[row+j];
+					}
+					else{
+						cost[i*height+j]+=cost[row+(j+1)];
+					}
+					// cost[i*height+j]+=min(cost[row+j],cost[row+j-1],cost[row+j+1]);
+				}
+				else if(j==0){
+					// cost[i*height+j]+=min(cost[row+j],cost[row+j+1]);
+					if(cost[row+j].norm()<cost[row+j+1].norm()){
+						cost[i*height+j]+=cost[row+j];
+					}
+					else{
+						cost[i*height+j]+=cost[row+j+1];
+					}
+				}
+				else{
+					// cost[i*height+j]+=min(cost[row+j],cost[row+j-1]);
+					if(cost[row+j].norm()<cost[row+j-1].norm()){
+						cost[i*height+j]+=cost[row+j];
+					}
+					else{
+						cost[i*height+j]+=cost[row+j-1];
+					}
+				}
+			}
+		}
+	}
+}
+void calcCost(double *energy,double *cost,int width,int height){
+	double c;
+	for (unsigned int i=0; i<height; i++) {
+		for (unsigned int j=0; j<width; j++) {
+			c=energy[i*width+j];
+			if(i==0){}
+		}
+	}
+}
+void energyCalc(Eigen::Vector3d *energy,double *e,Eigen::Vector3d *image,int width,int height){
 	Eigen::Vector3d x,y;
 	for (unsigned int i=0; i<width; i++) {
 		for (unsigned int j=0; j<height; j++) {
@@ -52,6 +117,7 @@ void energyCalc(Eigen::Vector3d *energy,Eigen::Vector3d *image,int width,int hei
 				y/=2;
 			}
 			energy[i*height+j]=(x.array().pow(2)+y.array().pow(2)).sqrt();
+			e[i*height+j]=energy[i*height+j].norm();
 		}
 	}
 }
@@ -61,6 +127,7 @@ int main(int argc, char *argv[]) {
 	Eigen::Vector3d *image = new Eigen::Vector3d[input.width()*input.height()];
 	Eigen::Vector3d *energy =new Eigen::Vector3d[input.width()*input.height()];
 	Eigen::Vector3d *grey =new Eigen::Vector3d[input.width()*input.height()];
+	double *eng=new double[input.width()*input.height()];
 	for (unsigned int i=0; i<input.width(); i++) {
 		for (unsigned int j=0; j<input.height(); j++) {
 			image[i*input.height()+j][0] = lab(i, j, 0);
@@ -69,20 +136,14 @@ int main(int argc, char *argv[]) {
 		}
 	}
 	//Calculate energy for the greyscale image
-	energyCalc(energy,image,input.width(),input.height());
-	Eigen::Vector3d max_energy(0.0,0.0,0.0);
+	energyCalc(energy,eng,image,input.width(),input.height());
+	// Eigen::Vector3d max_energy(0.0,0.0,0.0);
+	double max_energy=0;
 	//find max_energy
 	for(int i=0;i<input.width();i++){
 		for(int j=0;j<input.height();j++){
-			if(energy[i*input.height()+j][0]>max_energy[0]){
-				// std::cout<<"max"<<max_energy[0]<<"\tcurr"<<energy[i*height+j][0]<<std::endl;
-				max_energy[0]=energy[i*input.height()+j][0];
-			}
-			if(energy[i*input.height()+j][1]>max_energy[1]){
-				max_energy[1]=energy[i*input.height()+j][1];
-			}
-			if(energy[i*input.height()+j][2]>max_energy[2]){
-				max_energy[2]=energy[i*input.height()+j][2];
+			if(eng[i*input.height()+j]>max_energy){
+				max_energy=eng[i*input.height()+j];
 			}
 		}
 	}
@@ -90,9 +151,7 @@ int main(int argc, char *argv[]) {
 	//fill in grey with the greyscaled values
 	for(unsigned int i=0;i<input.width();i++){
 		for(unsigned int j=0;j<input.height();j++){
-			enrg[0]=energy[i*input.height()+j][0]/max_energy[0];
-			enrg[1]=energy[i*input.height()+j][1]/max_energy[1];
-			enrg[2]=energy[i*input.height()+j][2]/max_energy[2];
+			enrg=energy[i*input.height()+j]/max_energy;
 			grey[i*input.height()+j][0]=pow(enrg[0],1.0/3.0);
 			grey[i*input.height()+j][1]=pow(enrg[1],1.0/3.0);
 			grey[i*input.height()+j][2]=pow(enrg[2],1.0/3.0);
@@ -110,10 +169,66 @@ int main(int argc, char *argv[]) {
 	//convert greyScale to RGB, save to grey.jpg
 	CImg<double> greyImg=greyScale.LabtoRGB();
 	greyImg.save_jpeg("grey.jpg");
-
+	std::cout<<"starting shrink"<<std::endl;
+	bool done=false;
+	int height=input.height(),width=input.width();
+	Eigen::Vector3d *cost= new Eigen::Vector3d[input.width()*input.height()];
+	std::vector<int> vseam;
+	int index,l,r,m;
+	double min_cost;
+	while(!done){
+		std::cout<<width<<std::endl;
+		if(width>atoi(argv[3])){
+			energyCalc(energy,eng,image,width,height);
+			std::cout<<"Compute Cost"<<std::endl;
+			computeCost(energy,cost,width,height);
+			std::cout<<"Cost computed"<<std::endl;
+			for(int i=width-1;i>=0;i--){
+				min_cost=DBL_MAX;
+				index=0;
+				for(int j=height-1;j>=0;j--){
+					if((min_cost>cost[i*height+j].norm())&&(i==width-1||(i*height+j==l||i*height+j==m||i*height+j==r))){
+						min_cost=cost[i*height+j].norm();
+						index=i*height+j;
+						l=(i-1)*height+j-1;
+						m=(i-1)*height+j;
+						r=(i-1)*height+j+1;
+						// std::cout<<"big if true"<<std::endl;
+					}
+				}
+				// std::cout<<"i"<<i<<"\tindex\t"<<index<<std::endl;
+				vseam.push_back(index);
+			}
+			std::cout<<"seam created\tseam size"<<vseam.size()<<" "<<vseam[0]<<std::endl;
+			Eigen::Vector3d *temp;
+			temp=image;
+			image=new Eigen::Vector3d[(width)*height];
+			for(int i=0;i<input.width();i++){
+				index=vseam[vseam.size()-1];
+				vseam.pop_back();
+				// if(i==0) std::cout<<index<<std::endl;
+				for(int j=0;j<height;j++){
+					if(index!=i*height+j){
+						image[i*height+j]=temp[i*height+j];
+					}
+					else{
+						// std::cout<<index<<std::endl;
+						image[i*height+j]=Eigen::Vector3d(53.23,80.11,67.22);
+					}
+				}
+			}
+			std::cout<<"seam removed"<<std::endl;
+			delete [] temp;
+			width--;
+		}
+		if(width==atoi(argv[3])&&height==atoi(argv[4])){
+			done=true;
+		}
+	}
 	//begin seam finding
-	CImg<double> output(atoi(argv[3]), atoi(argv[4]), input.depth(), input.spectrum(), 0);
+	CImg<double> output(input.width(), input.height(), input.depth(), input.spectrum(), 0);
 	for (unsigned int i=0; i<output.width(); i++) {
+		// std::cout<<"i"<<i<<std::endl;
 		for (unsigned int j=0; j<output.height(); j++) {
 			output(i, j, 0) = image[i*output.height()+j][0];
 			output(i, j, 1) = image[i*output.height()+j][1];
@@ -126,7 +241,8 @@ int main(int argc, char *argv[]) {
 		rgb.save_png(argv[2]);
 	else if (strstr(argv[2], "jpg"))
 		rgb.save_jpeg(argv[2]);
-
+	delete [] eng;
+	delete [] cost;
 	delete [] energy;
 	delete [] grey;
 	delete [] image;
